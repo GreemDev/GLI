dad8f1487c5dec192d7f005a3a4b61b0928a3dbb
GreemDev <greem@greemdev.net>
2025-03-18 18:30:47 -0500

    fix arm windows binaries not being exes

--- publish.sh
+++ publish.sh
@@ -5,9 +5,9 @@ mv src/bin/Release/net9.0/linux-x64/publish/GitLabCli buildOutput/GitLabCli-linu
 dotnet publish -c Release -r linux-arm64
 mv src/bin/Release/net9.0/linux-arm64/publish/GitLabCli buildOutput/GitLabCli-linux_arm64
 dotnet publish -c Release -r win-x64
-mv src/bin/Release/net9.0/win-x64/publish/GitLabCli buildOutput/GitLabCli-win_x64
+mv src/bin/Release/net9.0/win-x64/publish/GitLabCli.exe buildOutput/GitLabCli-win_x64.exe
 dotnet publish -c Release -r win-arm64
-mv src/bin/Release/net9.0/win-arm64/publish/GitLabCli buildOutput/GitLabCli-win_arm64
+mv src/bin/Release/net9.0/win-arm64/publish/GitLabCli.exe buildOutput/GitLabCli-win_arm64.exe
 dotnet publish -c Release -r osx-arm64
 mv src/bin/Release/net9.0/osx-arm64/publish/GitLabCli buildOutput/GitLabCli-mac_arm64
 dotnet publish -c Release -r osx-x64
