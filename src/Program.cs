6b51c34798e8517e7b8efd9cea25787df2b1e36a
GreemDev <greemdev@ryujinx.app>
2025-06-19 22:35:21 -0500

    FUCK LOL

--- src/Program.cs
+++ src/Program.cs
@@ -7,17 +7,10 @@ Logger.OutputLogToStandardOut();
 
 Logger.Info(LogSource.App, "1");
 
-var currentPage = 2;
-do
-{
-    Logger.Info(LogSource.App, currentPage.ToString());
-    currentPage++;
-} while (currentPage <= 2);
-
-/*await Parser.Default.ParseArguments<Options>(args)
+await Parser.Default.ParseArguments<Options>(args)
     .WithNotParsed(errors =>
     {
         Logger.Error(LogSource.Cli, "Error parsing command-line arguments:");
         errors.ForEach(err => Logger.Error(LogSource.Cli, $" - {err.Tag}"));
     })
-    .WithParsedAsync(CliCommandManager.DispatchAsync);*/
+    .WithParsedAsync(CliCommandManager.DispatchAsync);
